ade729f107370f7a30d9151368119fe956708059
Emc54 <emilioschristou1@gmail.com>
2023-10-27 22:49:39 +0100

    customised parts of the theme

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1698442749';
+const cacheName = 'chirpy-1698443156';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
