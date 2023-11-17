faf87898d837b4104b5fe76a1045949de51b2b29
Emc54 <emilioschristou1@gmail.com>
2023-11-17 12:13:21 +0000

    updated site to include missing js

--- assets/js/data/swcache.js
+++ assets/js/data/swcache.js
@@ -28,14 +28,14 @@ const resource = [
     '/assets/img/favicons/favicon.ico',
     '/assets/img/favicons/mstile-150x150.png',
     '/assets/img/favicons/safari-pinned-tab.svg',
+    '/assets/img/favicons/android-chrome-192x192.png',
+    '/assets/img/favicons/android-chrome-512x512.png',
     '/assets/js/dist/categories.min.js',
     '/assets/js/dist/commons.min.js',
     '/assets/js/dist/home.min.js',
     '/assets/js/dist/misc.min.js',
     '/assets/js/dist/page.min.js',
-    '/assets/js/dist/post.min.js',
-    '/assets/img/favicons/android-chrome-192x192.png',
-    '/assets/img/favicons/android-chrome-512x512.png'
+    '/assets/js/dist/post.min.js'
 ];
 
 /* The request url with below domain will be cached */
