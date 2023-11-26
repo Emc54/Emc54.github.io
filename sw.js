b65351ee4a4a3cc880a6624cf2c125c6908f7aed
Emc54 <35582806+Emc54@users.noreply.github.com>
2023-11-26 22:11:54 +0000

    updated LED post (missing images)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1700963152';
+const cacheName = 'chirpy-1701036415';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
