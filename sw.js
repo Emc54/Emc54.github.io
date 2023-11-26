97ef0a464681205abc986d3ae67744c6d2ce8021
Emc54 <35582806+Emc54@users.noreply.github.com>
2023-11-26 01:47:37 +0000

    Added CV

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1700960547';
+const cacheName = 'chirpy-1700963152';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
