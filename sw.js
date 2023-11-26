66442df449fe6faea06864c821c7e92d23edff01
Emc54 <emilioschristou1@gmail.com>
2023-11-26 01:09:45 +0000

    updated smart lights post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1700331796';
+const cacheName = 'chirpy-1700960547';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
