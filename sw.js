b603e3a691164a44e69556fedf489d8ba94dc43d
Emc54 <35582806+Emc54@users.noreply.github.com>
2023-11-27 18:46:16 +0000

    completed LED Sign post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701036415';
+const cacheName = 'chirpy-1701110605';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
