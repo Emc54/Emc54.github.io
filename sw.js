d31a1048c36ef1e98aeafb3ddae286f22ab9b4cd
Emc54 <emilioschristou1@gmail.com>
2024-01-15 22:42:44 +0000

    updated page for the new year

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701110605';
+const cacheName = 'chirpy-1705358150';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
